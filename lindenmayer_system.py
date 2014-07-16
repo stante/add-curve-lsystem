35edc2081962c606759671e1c137b2cd313d2a16
Alexander Stante <stante@gmail.com>
2014-07-16 18:00:21 +0200

    Rename function to apply_rules()

--- lindenmayer_system.py
+++ lindenmayer_system.py
@@ -225,7 +225,7 @@ class LindenmayerSystem(bpy.types.Operator):
         trans = Movement(direction)
         stack = []
         occ = count(settings.rule, 'F')
-        system = recursive_apply("F", settings.rule, settings.iterations)
+        system = apply_rules("F", settings.rule, settings.iterations)
 
         # Create new curve object
         curve = bpy.data.curves.new('LSystem', 'CURVE')
@@ -278,7 +278,7 @@ class LindenmayerSystem(bpy.types.Operator):
                 spline, trans = stack.pop()
                 continue
 
-def recursive_apply(start, rules, times):
+def apply_rules(start, rules, times):
     newstring = start
 
     for i in range(times):
